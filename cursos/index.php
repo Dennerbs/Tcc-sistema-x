b697db0f31cecbdca854b85fde36bbdbf7570e36
Gustavo <ggoes269@gmail.com>
2020-03-24 21:53:49 -0300

    alterações na tela inicial e login, background

--- cursos/index.php
+++ cursos/index.php
@@ -11,8 +11,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
-        body {
-            background-color: #81DAF5;
+        p,
+        h1 {
+            color: #000000
+        }
+
+        #1 {
+            background-color: #F5F5DC;
 
         }
     </style>
@@ -21,34 +26,42 @@
 <body background="imagens/sakura.jpg">
     <div class="container">
         <?php require_once("Cabecalho.php"); ?>
-        <div class="m-auto col-md-12 w-50">
-            <div id="carouselExampleIndicators" class="carousel slide align-middle mt-4 border border-success" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="imagens/Bandeira_Coxim.png" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="imagens/Bandeira_MS.png" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="imagens/Bandeira_BR.png" class="d-block w-100" alt="...">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron-fluid mt-4">
+                    <div class="container">
+                        <h1 class="display-2 text-center">Bem vindo!!</h1>
+                        <p class="lead text-center">Sistema x, o sistema academico das escolas de Coxim-MS</p>
                     </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
-
+            <div class="col-md-6">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mt-4">
+            <a href="Login.php" type="button" class="btn btn-outline-dark btn-lg btn-block">Entrar </a>
+            </div>
         </div>
     </div>
 
